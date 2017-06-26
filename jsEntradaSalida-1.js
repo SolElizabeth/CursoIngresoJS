a5b2c20b626313b84975c380bedfcb16eec9d4e8
SolElizabeth <solbudur.sb@gmail.com>
2017-06-26 12:28:43 -0300

    Add files via upload
    Ejercicios

---
+++ jsEntradaSalida-1.js
@@ -0,0 +1,10 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+function Mostar()
+{
+    var nombre = document.getElementById ("el nombre").value;
+    alert (nombre);
+
+} 
+	
+
+
